55e4cfc53cb3aca4254b084a1f30a5f1e05ab052
Saloni Sinha <salonin204@gmail.com>
2018-10-29 18:08:00 -0400

    Added general execution_To catch and show exception to the user

--- omod/src/main/java/org/openmrs/module/stlm/web/controller/TTestRestController.java
+++ omod/src/main/java/org/openmrs/module/stlm/web/controller/TTestRestController.java
@@ -1,5 +1,8 @@
 package org.openmrs.module.stlm.web.controller;
 
+import org.apache.commons.math3.exception.MaxCountExceededException;
+import org.apache.commons.math3.exception.NullArgumentException;
+import org.apache.commons.math3.exception.NumberIsTooSmallException;
 import org.apache.commons.math3.stat.inference.TTest;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -16,11 +19,30 @@ public class TTestRestController {
 	@RequestMapping(method = RequestMethod.GET)
 	@ResponseBody
 	public String getTTest(@RequestParam("sample1") String[] sample1, @RequestParam("sample2") String[] sample2) {
-		String response;
-		double[] arr1 = Arrays.stream(sample1).mapToDouble(Double::parseDouble).toArray();
-		double[] arr2 = Arrays.stream(sample2).mapToDouble(Double::parseDouble).toArray();
-		TTest ttest = new TTest();
-		response = ttest.homoscedasticTTest(arr1, arr2) + "," + ttest.homoscedasticT(arr1, arr2);
+		String response=null;
+		String[] sample1_data=sample1;
+		String[] sample2_data=sample2;
+		if ((sample1_data.equals("") || null != sample1_data )&& (sample2_data.equals("") || null != sample2_data)) {
+			try {
+				double[] arr1 = Arrays.stream(sample1).mapToDouble(Double::parseDouble).toArray();
+				double[] arr2 = Arrays.stream(sample2).mapToDouble(Double::parseDouble).toArray();
+				TTest ttest = new TTest();
+				response = ttest.homoscedasticTTest(arr1, arr2) + "," + ttest.homoscedasticT(arr1, arr2);
+			} catch (NullArgumentException e) {
+				response = e.getMessage();
+			} catch (IllegalArgumentException e) {
+				response = e.getMessage();
+			}
+			catch (MaxCountExceededException e) {
+				response = e.getMessage();
+			} catch (Exception ex) {
+				response = ex.getMessage();
+			}
+		}
+		else {
+			response="Samples cannot be empty";
+		}
 		return response;
+
 	}
 }
